3e21b0089751942c7a18d33be469d812d6a7b910
Kamil Banach <banachkamil@outlook.com>
2024-06-07 07:15:43 +0200

    fixes

--- PaymentService/PaymentHandler.cs
+++ PaymentService/PaymentHandler.cs
@@ -76,7 +76,8 @@ public class PaymentHandler
     private async Task Payment(Message message)
     {
         var rnd = new Random();
-        await Task.Delay(rnd.Next(_minDelay, _maxDelay) * 1000, Token);
+        if (rnd.Next(0, 3) == 0) await Task.Delay(rnd.Next(_minDelay, _maxDelay) * 1000, Token);
+        
         var result = rnd.Next(0, 2) switch
         {
             0 => SagaState.PaymentFailed,
